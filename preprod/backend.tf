e775e1b1c6f1e1416881173e109ee1a6909d5b8e
projetajc-group3 <97603894+projetajc-group3@users.noreply.github.com>
2022-01-12 18:57:42 +0100

    Update backend.tf

--- preprod/backend.tf
+++ preprod/backend.tf
@@ -3,6 +3,6 @@ terraform {
         bucket = "projetgrp3-terraform-backend"
         key = "projetgrp3-backend-preprod.tfstate"
         region                  = "us-east-1"
-        shared_credentials_file = "/Users/renaudsautour/Downloads/PROJET/terraform/credentials"
+        shared_credentials_file = "/var/lib/jenkins/workspace/credentials"
     }
 }
