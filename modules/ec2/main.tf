e8e62aca6429a028d6b4c88c754b2f38aa87c702
projetajc-group3 <97603894+projetajc-group3@users.noreply.github.com>
2022-01-13 14:30:15 +0100

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -13,24 +13,6 @@ resource "aws_instance" "myec2" {
     volume_size = var.ec2_vol_size
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install ansible -y",
-      "sleep 5",
-      "git clone https://github.com/projetajc-group3/docker_role_deploy.git",
-      "cd cd docker_role_deploy/",
-      "ansible-galaxy install -r roles/requirements.yml",
-      "ansible-playbook -i hosts.yml docker.yml"
-    ]
-    
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("/var/lib/jenkins/workspace/${var.ec2_key_name}.pem")
-      host        = self.public_ip
-    }
-  }
 
   vpc_security_group_ids = [var.sg_id]
 
