7ca1a643e0097733d7d6d8253e7b8f4748a3eec1
projetajc-group3 <97603894+projetajc-group3@users.noreply.github.com>
2022-01-13 16:05:02 +0100

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -17,6 +17,11 @@ resource "aws_instance" "myec2" {
               #!/bin/bash
               sudo apt update -y
               sudo apt install ansible git -y
+              sleep 5
+              git clone https://github.com/projetajc-group3/docker_role_deploy.git
+              cd docker_role_deploy
+              ansible-galaxy install -r roles/requirements.yml
+              ansible-playbook -i hosts.yml docker.yml
               EOF
               
   vpc_security_group_ids = [var.sg_id]
