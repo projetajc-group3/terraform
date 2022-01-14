11708829824c293ad2723d8d94ca4c428a0fdb7a
projetajc-group3 <97603894+projetajc-group3@users.noreply.github.com>
2022-01-14 10:23:19 +0100

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -13,20 +13,7 @@ resource "aws_instance" "myec2" {
     volume_size = var.ec2_vol_size
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install ansible git -y
-              sleep 5
-              git clone https://github.com/projetajc-group3/docker_role_deploy.git
-              ansible-galaxy install -r docker_role_deploy/roles/requirements.yml
-              ansible-playbook -i docker_role_deploy/hosts.yml docker_role_deploy/docker.yml
-              sudo rm -rf docker_role_deploy
-              git clone https://github.com/projetajc-group3/kubernetes_role_deploy.git
-              ansible-galaxy install -r kubernetes_role_deploy/roles/requirements.yml
-              ansible-playbook -i kubernetes_role_deploy/hosts.yml kubernetes_role_deploy/kubernetes.yml
-              sudo rm -rf kubernetes_role_deploy       
-              EOF
+  user_data = var.ec2_user_data
               
   vpc_security_group_ids = [var.sg_id]
 
