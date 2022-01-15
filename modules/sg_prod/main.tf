ac0b4b57fd4bdcf46089758f66b01d71e64af954
projetajc-group3 <97603894+projetajc-group3@users.noreply.github.com>
2022-01-15 19:51:45 +0100

    Update main.tf

--- modules/sg_prod/main.tf
+++ modules/sg_prod/main.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "mysg" {
 
   ingress {
     description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 30000
+    to_port          = 30000
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
